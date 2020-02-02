98e7bd4a182c09e4fcf24ff86bd19cf9bc88d6fa
Majcon <majcon94@gmail.com>
2020-02-02 02:36:11 +0100

    Update update4.php

--- install/resources/update4.php
+++ install/resources/update4.php
@@ -1,4 +1,4 @@
-<?php
+v<?php
 	set_time_limit(0);
 
 	// Version: 4.1.2
@@ -14,7 +14,7 @@
 	}else{
 		echo "Coś poszło nie tak:C Nie można zaktualizować config.php";
 	}
-	if(preg_match("/\/\/Czy ma zmieniać nazwę kanału i ustawiać liczbę nowych użytkowników 1 - tak 0 - nie.*\],(.*)\/\/Poke/s/\/\/Czy ma zmieniać nazwę kanału i ustawiać liczbę nowych użytkowników 1 - tak 0 - nie.*\],(.*)\/\/Poke/s", $fgc)){
+	if(preg_match("/\/\/Czy ma zmieniać nazwę kanału i ustawiać liczbę nowych użytkowników 1 - tak 0 - nie.*\],(.*)\/\/Poke/s", $fgc)){
 		
 		$fgc = preg_replace("/(\/\/Czy ma zmieniać nazwę kanału i ustawiać liczbę nowych użytkowników 1 - tak 0 - nie.*\],)(.*)(\/\/Poke)/s", "$1\n\n	//Points() Funkcja .\n		'functions_Points' => [\n			'on'	=> true,	//true - włączona false - wyłączona\n			'inst'	=> 2, //ID Instancji \n			'top_list' => true,	//Czy ma ustawiać TOP w opisie kanału.\n			'cid' => 1,	//ID kanału gdzie ma wyświetlać top.\n			'gid'		=> [\n				1, 2\n			],	//ID grupy, której ma nie wyświetlać w topce.\n			'cldbid'	=> [\n				1, 2, 3\n			],	//Client database id użytkowników, których ma nie wyświetlać w topce np. MusicBOT czy też ten bot.\n			'limit'		=> 20	//Limit osób, które ma wyświetlać w top.\n		],\n$3", $fgc);
 		echo "Dodano dwie opcje do config.php 'info' - która pozwala nam ustawić czy ma wysyłać informacje o tym, że użytkownik rozpoczął transmisję live oraz 'info_text' - która pozwala ustawić tekst wysyłany podczas odpalania transmisji";
